b3d501693f0b69b70b920237c8a05bf5ebb2ff86
aliiiu <ayokunumi99@gmail.com>
2022-03-16 16:13:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Prints the alphabet 10 times
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	int i;
-	
-	for (i = 65; i <= 90; i++)
+
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
