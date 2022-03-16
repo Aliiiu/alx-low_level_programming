0bbb86a12cb68317c9f289ba7e16768708f085fc
aliiiu <ayokunumi99@gmail.com>
2022-03-16 15:41:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char *wrd = "_putchar";
-	
+
 	while (*wrd)
 	{
 		_putchar(*wrd);
@@ -15,5 +15,5 @@ int main(void)
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
