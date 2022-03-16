4394ef0bde6f4c903fb02d977999d03fbf29ddf5
aliiiu <ayokunumi99@gmail.com>
2022-03-16 16:03:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	
+	for (i = 65; i <= 90; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
