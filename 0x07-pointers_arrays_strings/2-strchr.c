56cffcfb2e89e11bd2b437c4cb99a0f620365e21
aliiiu <ayokunumi99@gmail.com>
2022-03-28 16:55:36 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * *_strchr checks for the first occurence of the 
+ * character c in the string s
+ * @s: string s
+ * @c: char c
+ * return: returns a pointer to the first occurence of the character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	i = 0;
+	while (*(s+i) != '\0')
+	{
+		if (*(s+i) == c)
+			return((s+i));
+		i++;
+	}
+	return (NULL);
+}
